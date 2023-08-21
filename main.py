8bac5e763b850cdbf5dbe9ab3753e3859bdcd560
Dominik Geyer <domi.kirchzarten@web.de>
2023-08-21 15:53:55 +0200

    styled 3 pages frontend and backend

--- main.py
+++ main.py
@@ -17,40 +17,42 @@ def reset_prompt(i, selection):
 
 slides = ["01_lion.png", "02_owl.png", "03_fox.png", "04_tree1.png", "04_tree2.png", "04_tree3.png"]
 slides2 = ["01_lion.png", "02_owl.png", "03_fox.png", "04_tree1.png", "04_tree2.png", "04_tree3.png", "04_tree1.png", "04_tree2.png", "04_tree3.png"]
+slides3 = ["01_lion.png", "02_owl.png", "03_fox.png"]
 
 @app.route('/', methods=['GET', 'POST'])
 def initial():
-  return render_template('test.html', slides=slides)
+  return render_template('index.html', slides=slides)
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-  return render_template('test.html', slides=slides)
+  return render_template('index.html', slides=slides)
 
 
-@app.route('/pattern', methods=['POST'])
+@app.route('/pattern', methods=['POST', 'GET'])
 def pattern():
   id = request.form.get('select')
+  print("Picked Image " + id + ": " + str(slides[int(id)]))
   image_name = slides[int(id)]
   return render_template("pattern.html", slides=slides2, image_name=image_name)
 
 
-@app.route('/generate', methods=['POST', 'GET'])
-def generate():
-  img = Image.open('test-image.png')
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+  img = Image.open('static/images/fox_mandala.png')
 
   data = io.BytesIO()
   img.save(data, format='PNG')
   encoded_img_data = base64.b64encode(data.getvalue())
 
-  prompt_str = ""
-  for x in prompt:
-    prompt_str += x + ", "
-  print('Current Prompt: ' + prompt_str)
+  #prompt_str = ""
+  #for x in prompt:
+  #  prompt_str += x + ", "
+  #print('Current Prompt: ' + prompt_str)
 
-  return render_template('display.html', img_data=encoded_img_data.decode('utf-8'), prompt_str=prompt_str)
+  return render_template('result.html', img_data=encoded_img_data.decode('utf-8'))#, prompt_str=prompt_str)
 
 if __name__ == '__main__':
-  app.run()
-  #app.run(debug=True)
+  #app.run()
+  app.run(debug=True)
 
 
--- scripts2.js
+++ scripts2.js
@@ -4,6 +4,7 @@ var slides = slide_list.getElementsByClassName("slides");
 var item_list = document.getElementById("list");
 var items = item_list.getElementsByClassName("item");
 var item_clicked;
+var actives = 0;
 
 var allNum = slides.length;
 var slNum = slides.length - 1;
@@ -24,16 +25,26 @@ var is_playing = false;
 console.log("Slides: " + slNum + "; Selection Items: " + items.length);
 
 //EventListeners
-//Set Item Event Listeners
-for (var i = 0; i < items.length; i++) {
-  items[i].addEventListener("click", start);
-  console.log("Added EventListener: " + i);
+//Item Handler
+for (let i = 0; i < items.length; i++) {
+  items[i].addEventListener("click", itemEvent(items[i]));
+}
+//Item Handler
+function itemEvent(this_item) {
+  console.log("Item id: " + id + "; Item: " + this_item + "clicked!");
+  start(this_item);
+  if (!is_playing) {
+    is_playing = true;
+    play_btn.addEventListener("click", play);
+  }
 }
 
 //Play
 function play() {
+  resetValues;
   slider = setInterval(next, 1500);
   play_btn.removeEventListener("click", play);
+
 }
 
 //Stop
@@ -42,33 +53,32 @@ const stop_btn = btn.addEventListener("click", stop);
 
 //Initial Active Status
 function initState() {
-  for (i = 0; i < items.length; i++) {
-    //items[i].classList.add("activeList");
+  for (let i = 0; i < items.length; i++) {
+    items[i].classList.add("activeList");
     items[i].classList.add("active-border-blinker");
   }
 }
 document.getElementById("list").addEventListener("load", initState());
 
 //Start Interval
-function start() {
-  id = Array.from(this.parentNode.children).indexOf(this) - 1;
-  if (!is_playing) {
-    play_btn.addEventListener("click", play);
-  }
-  is_playing = true;
+function start(this_item) {
+  id = Array.from(this_item.parentNode.children).indexOf(this_item) - 1;
+  console.log("Id is: " + id);
   stop();
   resetStates(slides);
-  activeItem(this);
+  activeItem(this_item);
   //Show Initial Image
   slides[id * maxSteps].classList.add("active");
   //Start Interval
   slider = setInterval(next, 1500);
+  is_playing = true;
 }
 
 //Stop Interval
 function stop() {
   clearInterval(slider);
   play_btn.addEventListener("click", play);
+  is_playing = false;
 }
 
 function swapClass(elem, name) {
@@ -80,27 +90,27 @@ function swapClass(elem, name) {
   }
 }
 
-function activeItem(selectedItem) {
+//Set Active States
+function activeItem(currentItem) {
   //Remove Pre Selected Item Active Status
-  var actives = document.getElementsByClassName("item-active");
-  if (actives.length > 0) {
-    for(let i = 0; i < actives.length; i++) {
-      actives[i].classList.remove("item-active");
-    }
-    selectedItem.classList.remove("darken-state");
+  resetValues;
+  var elem = document.getElementsByClassName("item-active");
+  if (elem.length > 0) {
+    preItem = elem[0].classList;
+    preItem.remove("item-active");
+    currentItem.classList.remove("darken-state");
   }
+
   for (let i = 0; i < items.length; i++) {
     let item = items[i];
     item.classList.remove("active-border-blinker");
   }
-  //Set Current Item Active
-  selectedItem.classList.add("item-active");
-  //Darken Other Items
-  for (let i = 0; i < items.length; i++) {
-    if(i != id) {
-      items[i].classList.add("darken-state");
-    }
-  }
+
+  //Set Selected Item Active
+  currentItem.classList.add("item-active");
+  //Darken other
+  index_other = ((id + 1) % (items.length));
+  items[index_other].classList.add("darken-state");
 
   //Set Submit Button Active
   submitButton = document.getElementById("start").classList;
@@ -111,35 +121,31 @@ function activeItem(selectedItem) {
   document.getElementById("submit").type = "submit";
 }
 
+// Swipe Left or Right
 function swapImg(direction) {
   next(direction);
 }
 
 function next(direction) {
   resetValues;
+
   if (direction != null && direction == 1) {
-    //step left
+    //left
     index--;
     index = index < 0 ? maxSteps - 1 : index;
   } else {
-    //step right
+    //right
     index++;
   }
 
   slide_list = document.getElementById("mainImg-wrap");
   slides = slide_list.getElementsByClassName("slides");
 
-  //Current Slide
-  var slideIndex = (index % maxSteps) + indexInterval[id];
-  var current = slides[slideIndex];
+  var tempIndex = (index % maxSteps) + indexInterval[id];
+  var current = slides[tempIndex];
+  console.log("Active Image:" + tempIndex + "; Index: " + index);
 
-  //Pass Slide Value 
-  document.getElementById("value").value = slideIndex.toString();
-  console.log("Active Image:" + slideIndex + "; Index: " + index);
-
-  //Refresh All Active Slides
   resetStates(slides);
-  //Activate Current Slide
   swapClass(current, "active");
 }
 
@@ -161,12 +167,18 @@ function resetValues() {
   indexInterval = [0, maxSteps, maxSteps * 2];
 }
 
-function resetStates(activeItems) {
+function resetStates() {
   resetValues;
-  for (i = 0; i < allNum; i++) {
-    var curr = slides[i].classList;
-    if (curr.contains("active")) {
-      curr.remove("active");
+  for (let i = 0; i < allNum; i++) {
+    let curr_slide = slides[i].classList;
+    if (curr_slide.contains("active")) {
+      curr_slide.remove("active");
+    }
+  }
+  for (let i = 0; i < items.length; i++) {
+    let curr_item = items[i].classList;
+    if (curr_item.contains("darken-state")) {
+      curr_item.remove("darken-state");
     }
   }
 }
